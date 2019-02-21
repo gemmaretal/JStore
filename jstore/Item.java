1499e2bc39866dacc623516647c0e1449ac1201b
gemmaretal <44894758+gemmaretal@users.noreply.github.com>
2019-02-21 17:47:19 +0700

    modul 1

---
+++ jstore/Item.java
@@ -0,0 +1,70 @@
+
+/**
+ * Write a description of class Item here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Item
+{
+    protected int id;
+    protected String name;
+    protected int stock;
+    protected int price;
+    protected String category;
+
+    public Item()
+    {
+
+    }
+
+    public int getId()
+    {
+        return 0;
+    }
+    
+    public String getName()
+    {
+        return "";
+    }
+    
+    public int getStock()
+    {
+        return 0;
+    }
+    
+    public int getPrice()
+    {
+        return 0;
+    }
+    
+    public String getCategory()
+    {
+        return "";
+    }
+    
+    public int setId(int id)
+    {
+        return 0;
+    }
+    
+    public String setName(String name)
+    {
+        return "";
+    }
+    
+    public int setStock(int stock)
+    {
+        return 0;
+    }
+    
+    public int setPrice(int price)
+    {
+        return 0;
+    }
+    
+    public String setCategory(String category)
+    {
+        return "";
+    }
+}
---
+++ jstore/Supplier.java
@@ -0,0 +1,70 @@
+
+/**
+ * Write a description of class Supplier here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Supplier
+{
+    protected int id;
+    protected String name;
+    protected String email;
+    protected String phoneNumber;
+    protected String city;
+
+    public Supplier()
+    {
+
+    }
+
+    public int getid()
+    {
+        return 0;
+    }
+    
+    public String getname()
+    {
+        return "";
+    }
+    
+    public String getemail()
+    {
+        return "";
+    }
+    
+    public String getphoneNumber()
+    {
+        return "";
+    }
+    
+    public String getcity()
+    {
+        return "";
+    }
+    
+    public int setid(int id)
+    {
+        return 0;
+    }
+    
+    public String setname(String name)
+    {
+        return "";
+    }
+    
+    public String setemail(String email)
+    {
+        return "";
+    }
+    
+    public String setphoneNumber(String phoneNumber)
+    {
+        return "";
+    }
+    
+    public String setCity(String City)
+    {
+        return "";
+    }
+}
